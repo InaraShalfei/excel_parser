5ca3c2b0b7d5c7fdae87bef48ab07bc35a6b22ba
InaraShalfei <inarashugaikina@gmail.com>
2023-04-19 20:44:26 +0600

    Initially create parse_file view function to parse the file from project path and save data in database

---
+++ excel/excel_parser/views.py
@@ -0,0 +1,24 @@
+import datetime
+import os
+
+import pandas as pd
+import numpy as np
+
+from .models import UserModel
+
+
+def parse_file(request):
+    df = pd.read_excel('/home/user/PycharmProjects/excel_parser/excel/test.xlsx')
+    df = df.replace(np.nan, None)
+
+    for worker in df.to_dict('records'):
+        document_date = datetime.datetime.strptime(worker['Дата табеля'], '%d.%m.%Y')
+
+        UserModel.objects.create(name=worker['Сотрудник'], position=worker['Функция клиента'],
+                                 IIN=worker['ИНН сотрудника'],
+                                 company=worker['Контрагент'], planned_start_time=worker['Плановое начало смены'],
+                                 planned_finish_time=worker['Плановое окончание смены'],
+                                 company_branch=worker['Организация'],
+                                 real_start_time=worker['Отметка на приход'],
+                                 real_finish_time=worker['Отметка на уход'],
+                                 document_date=document_date)
